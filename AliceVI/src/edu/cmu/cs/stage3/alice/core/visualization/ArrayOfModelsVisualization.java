9a90cddc98e49b43f94ddd8e014db4a268dc689c
Alice <Alice@0df813c8-cd06-51d8-4955-4e1088891cc4>
2009-12-17 17:51:13 +0000

    Initial Import

---
+++ AliceVI/src/edu/cmu/cs/stage3/alice/core/visualization/ArrayOfModelsVisualization.java
@@ -0,0 +1,7 @@
+package edu.cmu.cs.stage3.alice.core.visualization;
+
+public class ArrayOfModelsVisualization extends CollectionOfModelsVisualization {
+	protected String getItemsName() {
+		return "elements";
+	}
+}
